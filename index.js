62de094c71f6b95c13c38147657fca73da95e383
Sreekanth <116191030+sreekanthpsree@users.noreply.github.com>
2023-04-03 21:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,5 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
-}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
